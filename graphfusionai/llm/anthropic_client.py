b2d0baa55054c167fa7c2383ba2f92f05dff1906
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-02-11 17:30:04 +0300

    New

--- graphfusionai/llm/anthropic_client.py
+++ graphfusionai/llm/anthropic_client.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+import requests
 from .base_llm import BaseLLM
 
 class AnthropicClient(BaseLLM):
